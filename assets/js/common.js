4a657c66b0785e255574b90f4512bfa21595ce37
phantumblade <101518764+phantumblade@users.noreply.github.com>
2025-07-03 18:11:06 +0200

    Completa esempi e documenti, rimuove file inutili

---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+// Piccola funzione di utilità
+function ready(fn) {
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function() {
+    console.log('Pagina caricata');
+});
